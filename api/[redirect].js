d1d72e1104bcf9252f4bc212d815572f9b095a22
ReNeeter <69535871+reneeter123@users.noreply.github.com>
2021-06-11 05:09:38 +0900

    セミコロン

--- api/[redirect].js
+++ api/[redirect].js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-    const redirect = req.query.redirect.split(".")
+    const redirect = req.query.redirect.split(".");
     const now = new Date();
 
     res.redirect(302, `https://280blocker.net/files/280blocker_${redirect[0]}_${now.getFullYear()}${`0${now.getMonth() + 1}`.slice(-2)}.${redirect[1]}`);
